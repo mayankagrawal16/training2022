eb09e91cfc63ff9eeb8a2a6c97757ce0aed16ca8
Mayank Agrawal <mayank.agrawal1604@gmail.com>
2022-10-11 18:20:19 +0530

    11-october

--- day_15/day_16.1.py
+++ day_15/day_16.1.py
@@ -1,41 +1,64 @@
 import tkinter as ttk
 import pandas as pd
-app = ttk.Tk()
-app.title("Recommendation System")
-app.geometry('400x400')
+import warnings
+warnings.filterwarnings('ignore')
+app=ttk.Tk()
+app.title("Recommendaion System")
+app.geometry('500x400')
 
-cols=['user_id','movie_id','rating','ts']
-item_cols=['movie_id','title']+[str(i) for i in range(22)]
-df=pd.read_csv('u.data',sep='\t',names=cols)
-df1=pd.read_csv('u.item',sep='|',encoding='ISO-8859-1',names=item_cols)
-movie=pd.merge(df,df1,on='movie_id')
 
-result = ttk.Variable(app)
+cols = ['user_id','movie_id','rating','ts']
+item_cols=['movie_id','title'] + [str(i) for i in range(22)]
+df=pd.read_csv('u.data', sep='\t',names=cols)
+df1=pd.read_csv('u.item',sep='|',encoding= "ISO-8859-1",names=item_cols)[['movie_id','title']]
+movie= pd.merge(df,df1,on='movie_id')
+
+result=ttk.Variable(app)
+
 
 frame=ttk.Frame(app)
 frame.place(x=10,y=10)
+box=ttk.Listbox(frame,height=10,width=50)
 
-box = ttk.Listbox(frame,height=10,width=50)
 for title in movie['title'].unique():
     box.insert(ttk.END,title)
 #box.grid(row=0,column=0)
 box.pack(side='left',fill='y')
-#box.place(x=10,y=10)
+# box.place(x=10,y=10)
 
-scroll = ttk.Scrollbar(frame,orient=ttk.VERTICAL)
-scroll.config(command = box.yview)
+scroll=ttk.Scrollbar(frame,orient=ttk.VERTICAL)
+scroll.config(command=box.yview)
 box.config(yscrollcommand=scroll.set)
 scroll.pack(side='right',fill='y')
 
-
-#for row,val in movie.iterrows():
-   # box.insert(row+1,val['title'])
-
-box.place(x=10,y=10)
-
 def get_movie():
-    pass
-ttk.Button(app, text="Find Recommendations", font=('Arial',22),command=get_movie).place(x=200,y=50)
-ttk.Label(app, textvariable=result,font=('Arial',22)).place(x=200,y=100)
+    movie_selected=box.get(box.curselection())
+    print('movie_selected',movie_selected)
+    # Create Pivot Table
+    movie_pivot=movie.pivot_table(index='user_id',columns='title',values='rating')
+    
+    # Find Similarity for selected movie
+    corrs=movie_pivot.corrwith(movie_pivot[movie_selected])
+    corrs_df=pd.DataFrame(corrs,columns=['correlation'])
+    corrs_df['rating']=movie.groupby('title')['rating'].mean()
+    corrs_df['count']=movie['title'].value_counts()
+    
+    # Find top 2-3 Recommendations
+    top_recom=list(corrs_df[corrs_df['count']>50].sort_values(by='correlation',ascending=False).head(3).index)
+    
+    
+    ## IMPORTANT
+    if movie_selected in top_recom:
+        top_recom.remove(movie_selected)
+    print('Recommendations:',top_recom)
+
+    
+    if top_recom:
+        result.set(top_recom[0])
+    else:
+        result.set("Sorry No Recommendations Found !")
+
+ttk.Button(app,text='Find Recommendations',command=get_movie,font=('Arial',22)).place(x=10,y=250)
+ttk.Label(app, textvariable=result,font=('Arial',22)).place(x=10,y=300)
 
 app.mainloop()
